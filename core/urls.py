637e45c038fc8fc701ef22fbb6e47f974da43b39
bettina;isa shinaven <bettinashinavene@gmail.com>
2024-09-12 23:31:12 +0200

    Your commit message

--- core/urls.py
+++ core/urls.py
@@ -11,4 +11,4 @@ urlpatterns = [
  path('contact/', views.contact, name='contact'),
  path('signup/', views.signup, name='signup'),
  path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
-]
+  ]
